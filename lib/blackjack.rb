ae32cb2f43149424c05ed51e50d7e04e24071c9b
Kevin Glasgow <kglas001@gmail.com>
2020-10-23 22:53:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,10 +61,12 @@ end
 
 def runner
   welcome
-  total = initial_round
-  while total < 21
-    card_total = hit?(total)
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
   end_game(card_total)
 end
     
+runner
